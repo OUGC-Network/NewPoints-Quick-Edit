6ef2f8a0c6cda235928f20dcb655aacfad02f63a
Sama34 <Sama34@users.noreply.github.com>
2024-08-03 00:55:57 -0700

    Code style PSR12

--- Upload/inc/plugins/newpoints/languages/english/newpoints_quickedit.lang.php
+++ Upload/inc/plugins/newpoints/languages/english/newpoints_quickedit.lang.php
@@ -2,30 +2,29 @@
 
 /***************************************************************************
  *
- *   Newpoints Quick Edit plugin (/inc/plugins/newpoints/languages/english/newpoints_quickedit.php)
- *	 Author: Omar Gonzalez
- *   Copyright: © 2012 Omar Gonzalez
- *   
- *   Website: http://community.mybb.com/user-25096.html
+ *    Newpoints Quick Edit plugin (/inc/plugins/newpoints/languages/english/newpoints_quickedit.lang.php)
+ *    Author: Omar Gonzalez
+ *    Copyright: Â© 2012 Omar Gonzalez
  *
- *   Quickly edit user's points without accessing to the ACP.
+ *    Website: https://ougc.network
+ *
+ *    Quickly edit user's points without accessing to the ACP.
  *
  ***************************************************************************
- 
-****************************************************************************
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+ ****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 
 $l['quickedit_edit_points'] = 'Edit User';
 $l['quick_edit'] = 'Quick Edit';
