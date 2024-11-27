aa07822f18c921ed16597c251d52e9e3ef8b45b7
Sama34 <Sama34@users.noreply.github.com>
2024-11-27 08:44:20 -0800

    Spanish language wip

---
+++ Upload/inc/plugins/newpoints/languages/espanol/admin/newpoints_quickedit.lang.php
@@ -0,0 +1,34 @@
+<?php
+
+/***************************************************************************
+ *
+ *    Newpoints Quick Edit plugin (/inc/plugins/newpoints/languages/english/admin/newpoints_quickedit.lang.php)
+ *    Author: Omar Gonzalez
+ *    Copyright: © 2012 Omar Gonzalez
+ *
+ *    Website: https://ougc.network
+ *
+ *    Quickly edit user's Newpoints data from the forums.
+ *
+ ***************************************************************************
+ ****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+$l = [
+    'newpoints_quick_edit' => 'Quick Edit',
+    'newpoints_quick_edit_desc' => "Quickly edit user's Newpoints data from the forums.",
+
+    'newpoints_user_groups_quick_edit_can_use' => 'Can quick edit users?',
+];
---
+++ Upload/inc/plugins/newpoints/languages/espanol/newpoints_quickedit.lang.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************************
+ *
+ *    Newpoints Quick Edit plugin (/inc/plugins/newpoints/languages/english/newpoints_quick_edit.lang.php)
+ *    Author: Omar Gonzalez
+ *    Copyright: © 2012 Omar Gonzalez
+ *
+ *    Website: https://ougc.network
+ *
+ *    Quickly edit user's Newpoints data from the forums.
+ *
+ ***************************************************************************
+ ****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+$l = [
+    'newpoints_quick_edit' => 'Edicion Rapida',
+
+    'newpoints_quickedit_newpoints_menu' => 'Edicion Rapida',
+
+    'newpoints_quick_edit_page_title' => 'Editar Usiario',
+    'newpoints_quick_edit_page_nav' => 'Editar Usiario',
+
+    'newpoints_quick_edit_table_title' => 'Editando a {1}',
+    'newpoints_quick_edit_table_description' => 'Aqui puedes editar la informacion de puntos del usuario.',
+    'newpoints_quick_edit_table_edit_title' => 'Editar puntos',
+    'newpoints_quick_edit_table_edit_description' => 'Cualquier cantidad ingresada sera agregada a los puntos actuales. Usa un signo negativo para restar puntos.',
+    'newpoints_quick_edit_table_current' => 'Puntos actuales',
+
+    'newpoints_quick_edit_post_bit' => 'Editar datos de puntos',
+
+    'newpoints_quick_edit_redirect_successful' => 'El usuario fue actualizado exitosamente.<br/><br/>Ahora seras redirigido a la pagina anterior.',
+];
